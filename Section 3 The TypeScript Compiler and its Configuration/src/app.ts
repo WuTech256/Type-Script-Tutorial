97233d79e77d85bbb92dbc2afd18df88ef49d1d6
WuTech256 <vutoanlearning@gmail.com>
2025-03-21 18:34:59 +0700

    Create app.ts

---
+++ Section 3 The TypeScript Compiler and its Configuration/src/app.ts
@@ -0,0 +1,13 @@
+import fs from 'node:fs';
+
+// fs.readFileSync()
+let userName: string;
+userName = 'Max';
+
+console.log(userName);
+
+function add(a: any, b: any) {
+    return a + b;
+}
+
+add(5, 10);
