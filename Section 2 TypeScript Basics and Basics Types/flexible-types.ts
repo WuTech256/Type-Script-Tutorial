24e3a8d871e8a17c04975d4d76f57db5a70ab32c
WuTech256 <vutoanlearning@gmail.com>
2025-03-21 18:30:45 +0700

    Create flexible-types.ts

---
+++ Section 2 TypeScript Basics and Basics Types/flexible-types.ts
@@ -0,0 +1,14 @@
+// The 'any' Type
+let age: any = 36;
+
+// ...
+
+age = '37';
+age = false;
+age  = {};
+age = [];
+
+// Understanding Union Types.
+let age_1: string | number = 36;
+
+age_1 = '397';
